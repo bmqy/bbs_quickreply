20fe6f2345aede124cebd4e48cd1b63df82486f1
bmqy <qq88268459@126.com>
2017-12-28 15:40:43 +0800

    bbs_quickreply
    bbs_quickreply

--- chrome/js/bbs_quickreply.user.js
+++ chrome/js/bbs_quickreply.user.js
@@ -1,40 +1,64 @@
 (function() {
     'use strict';
+    function QuickReply(){
 
-    var defaultArr = [
-        '感谢楼主分享，支持一下！',
-        '支持一下，希望楼主做的更好，加油！',
-        '做的不错哦，楼主加油，期待更好的作品！',
-        '标记一下，先看看好不好，谢谢楼主咯！'
-    ];
-    // 预定义回复内容
-    var aReplys = defaultArr;
-    if(localStorage.getItem('replysCustom') && localStorage.getItem('replysCustom').toString().trim() !== ''){
-		aReplys = new Array(localStorage.getItem('replysCustom'));
+        // 预定义回复内容
+        this.replysDefault = [
+            '感谢楼主分享，支持一下！',
+            '支持一下，希望楼主做的更好，加油！',
+            '做的不错哦，楼主加油，期待更好的作品！',
+            '标记一下，先看看好不好，谢谢楼主咯！'
+        ];
+
+        // 自定义回复内容
+        this.replysCustom = (localStorage.getItem('replysCustom') && localStorage.getItem('replysCustom').toString().trim() !== '') ? (localStorage.getItem('replysCustom')).split(',') : this.replysDefault;
+
+        // 源回复框
+        this.targetReplyEditWarp = document.querySelector('#fastposteditor');
+        this.targetReplyEditContent = document.querySelector('#fastpostmessage');
     }
 
-    // 更新快捷回帖列表
-    function quickReplysUpdate(obj, arrReplys){
-        obj.options.length = 0;
-        for(var i=0;i<arrReplys.length;i++){
-            obj.options.add(new Option(arrReplys[i], i));
-        }
-    }
+    QuickReply.prototype.getQuickReply = function(){
+        var oQuikReplyWarp = document.createElement('div');
 
-    // 更新自定义回帖内容
-    function quickReplysCustomUpdate(obj, arrReplys){
-        var _tempAReplys = '';
-        for(var i=0;i<arrReplys.length; i++){
-            _tempAReplys += arrReplys[i] +'\n';
-        }
-        obj.value = _tempAReplys;
-    }
+        var oQuikReplyLabel = document.createElement('label');
+        oQuikReplyLabel.style = 'color:red;font-weight:bolder';
+        oQuikReplyLabel.innerHTML = '快捷回帖：';
 
-    // 理论上支持任意Discuz类论坛
-    if(document.querySelector('#fastposteditor')){
+        var oQuikReplySelect = document.createElement('select');
+        oQuikReplySelect.id = 'quickReplyList';
+        QuickReply.updateReplysSelect(oQuikReplySelect, QuickReply.replysCustom);
 
-        // 自定义回复
+        oQuikReplySelect.addEventListener('change', function(e){
+            QuickReply.targetReplyEditContent.value = this.options[this.selectedIndex].text;
+            QuickReply.targetReplyEditContent.style.background = 'none';
+        });
+
+        var oQuickReplyBtnCustom = document.createElement('a');
+        oQuickReplyBtnCustom.innerHTML = '自定义回帖';
+        oQuickReplyBtnCustom.href = 'javascript:;';
+        oQuickReplyBtnCustom.style = "margin-left:10px;color:#999;font-size:12px;";
+
+        oQuickReplyBtnCustom.addEventListener('click', function(e){
+            var _posX = e.clientX;
+            var _posY = e.clientY;
+            var oQuickReplyCustom = document.querySelector('#quickReplyCustom');
+
+            oQuickReplyCustom.style.top = _posY - 230 +'px';
+            oQuickReplyCustom.style.left = _posX - 150 +'px';
+            oQuickReplyCustom.style.display = 'block';
+        });
+
+        oQuikReplyLabel.appendChild(oQuikReplySelect);
+        oQuikReplyWarp.appendChild(oQuikReplyLabel);
+        oQuikReplyWarp.appendChild(oQuickReplyBtnCustom);
+
+        QuickReply.targetReplyEditWarp.insertBefore(oQuikReplyWarp, QuickReply.targetReplyEditWarp.childNodes[0]);
+    };
+
+    QuickReply.prototype.getReplysCustom = function(){
         var oCustomPanel = document.createElement('div');
+        oCustomPanel.id = 'quickReplyCustom';
         oCustomPanel.style = 'width:280px;height:190px;text-align:center;background-color: #fff;padding:10px;box-shadow:0 0 3px 5px #ddd;position:fixed;left:50%;top:13%;display:none;';
 
         var oCustomTips = document.createElement('p');
@@ -44,29 +68,29 @@
         var oCustomTextarea = document.createElement('textarea');
         oCustomTextarea.style = 'width:96%;padding:5px;';
         oCustomTextarea.rows = '7';
-        oCustomTextarea.id = 'quickReplyCustom';
-        quickReplysCustomUpdate(oCustomTextarea, aReplys);
+        QuickReply.updateReplysCustom(oCustomTextarea, this.replysCustom);
 
         var oCustomSaveBtn = document.createElement('button');
         oCustomSaveBtn.style = 'width: 100%;';
         oCustomSaveBtn.innerHTML = '保存';
-        oCustomSaveBtn.id = 'customReplySaveBtn';
 
         oCustomSaveBtn.addEventListener('click', function(){
+            var oQuickReplySelect = document.querySelector('#quickReplyList');
             var _customReply = oCustomTextarea.value.split('\n');
             var _tempArr = [];
+
             for(var i=0; i<_customReply.length; i++){
                 if(_customReply[i] !== ''){
                     _tempArr.push(_customReply[i]);
                 }
             }
             if(_tempArr.length === 0){
-                _tempArr = defaultArr;
+                _tempArr = QuickReply.replysDefault;
             }
-
             oCustomPanel.style.display = 'none';
-            quickReplysCustomUpdate(oCustomTextarea, _tempArr);
-            quickReplysUpdate(oQuikReplyList, _tempArr);
+            QuickReply.updateReplysCustom(oCustomTextarea, _tempArr);
+            QuickReply.updateReplysSelect(oQuickReplySelect, _tempArr);
+            QuickReply.targetReplyEditContent.value = _tempArr[0];
             localStorage.setItem('replysCustom', _tempArr);
         });
 
@@ -74,48 +98,33 @@
         oCustomPanel.appendChild(oCustomTips);
         oCustomPanel.appendChild(oCustomSaveBtn);
         document.body.appendChild(oCustomPanel);
+    };
 
-        // 载入快捷回帖
-        var oReplyEdit = document.querySelector('#fastposteditor');
-        var oReplyEditContent = document.querySelector('#fastpostmessage');
-        var oQuikReplyWarp = document.createElement('div');
-
-        var oQuikReplyLabel = document.createElement('label');
-        oQuikReplyLabel.style = 'color:red;font-weight:bolder';
-        oQuikReplyLabel.innerHTML = '快捷回帖：';
-
-        var oQuikReplyList = document.createElement('select');
-        oQuikReplyList.id = 'quickReply';
-        for(var j=0;j<aReplys.length;j++){
-            oQuikReplyList.options.add(new Option(aReplys[j], j));
+    QuickReply.prototype.updateReplysSelect = function (obj, arrReplys){
+        obj.options.length = 0;
+        for(var i=0;i<arrReplys.length;i++){
+            obj.options.add(new Option(arrReplys[i], i));
         }
+    };
 
-        var oQuickReplyBtnCustom = document.createElement('a');
-        oQuickReplyBtnCustom.innerHTML = '自定义回复';
-        oQuickReplyBtnCustom.href = 'javascript:;';
-        oQuickReplyBtnCustom.style = "margin-left:10px;color:#999;font-size:12px;";
-        oQuickReplyBtnCustom.id = 'btnCustomReply';
+    QuickReply.prototype.updateReplysCustom = function(obj, arrReplys){
+        var _tempAReplys = '';
+        for(var i=0;i<arrReplys.length; i++){
+            _tempAReplys += arrReplys[i] +'\n';
+        }
+        obj.value = _tempAReplys;
+    };
 
-        oQuikReplyLabel.appendChild(oQuikReplyList);
-        oQuikReplyWarp.appendChild(oQuikReplyLabel);
-        oQuikReplyWarp.appendChild(oQuickReplyBtnCustom);
-        oReplyEdit.insertBefore(oQuikReplyWarp, oReplyEdit.childNodes[0]);
+    QuickReply.prototype.init = function(){
+        // 理论上支持大部分Discuz类论坛
+        if(document.querySelector('#fastposteditor')){
+            QuickReply.getQuickReply();
+            QuickReply.getReplysCustom();
+            QuickReply.targetReplyEditContent.value = QuickReply.replysCustom[0];
+            QuickReply.targetReplyEditContent.style.background = 'none';
+        }
+    };
 
-        oReplyEditContent.value = aReplys[0];
-        oReplyEditContent.style.background = 'none';
-
-        oQuikReplyList.addEventListener('change', function(){
-            oReplyEditContent.value = this.options[this.selectedIndex].text;
-            oReplyEditContent.style.background = 'none';
-        });
-
-        oQuickReplyBtnCustom.addEventListener('click', function(e){
-            var _posX = e.clientX;
-            var _posY = e.clientY;
-
-            oCustomPanel.style.top = _posY - 230 +'px';
-            oCustomPanel.style.left = _posX - 150 +'px';
-            oCustomPanel.style.display = 'block';
-        });
-    }
+    var QuickReply = new QuickReply();
+    QuickReply.init();
 })();
