1834b68fea607d91fb4456e70d409c494bbca1de
bmqy <qq88268459@126.com>
2024-11-12 14:42:25 +0000

    打包更新

--- dist/bbs_quickreply.user.js
+++ dist/bbs_quickreply.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         论坛快捷回帖
 // @namespace    bmqy.net
-// @version      3.7.2
+// @version      3.7.3
 // @author       bmqy
 // @description  使用自定义内容或本扩展预定义的回帖内容，快捷回复支持的论坛的发帖！
 // @license      ISC
@@ -22,6 +22,8 @@
 // @match        *://linux.do/*
 // @match        *://meta.appinn.net/*
 // @match        *://www.v2ex.com/t/*
+// @match        *://discuss.flarum.org/*
+// @match        *://www.nodeloc.com/*
 // @require      https://cdn.jsdelivr.net/npm/vue@3.3.4/dist/vue.global.prod.js
 // @require      data:application/javascript,%3Bwindow.Vue%3DVue%3B
 // @require      https://cdn.jsdelivr.net/npm/element-plus@2.3.6/dist/index.full.min.js
@@ -42,7 +44,7 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
-(a=>{const e=document.createElement("style");e.dataset.source="vite-plugin-monkey",e.textContent=a,document.head.append(e)})(' .quickReplyBox[data-v-029dc477]{position:relative}.quickReplyBox .el-form[data-v-029dc477]{text-align:left}.quickReplyBox .reply-form-inline .el-form-item[data-v-029dc477]{margin-bottom:15px}.el-dialog__footer a{text-decoration:none}.el-tabs__nav-next,.el-tabs__nav-prev{height:44px;display:flex;justify-content:center;align-items:center}.el-dialog{display:flex;flex-direction:column;margin:0!important;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);max-height:calc(100% - 30px);max-width:1300px;min-width:1000px}.el-dialog__header{margin-right:0!important}.el-dialog__body{flex:1;overflow:auto;padding:0}.quickReplyBox .el-input__inner{margin-bottom:0;background-color:transparent;border:0;outline:none}#reply-control.open{height:calc(var(--composer-height) + 65px)!important}#reply-control .reply-area{height:calc(100% - 91px)!important}.app-dialog-foot[data-v-029dc477]{color:#909399;font-size:14px}.quickReplyBoxTitle[data-v-029dc477]{margin-right:10px;font-weight:700;color:red}.el-form-item--mini.el-form-item[data-v-029dc477],.el-form-item--small.el-form-item[data-v-029dc477]{margin-bottom:10px}.el-select[data-v-029dc477]{width:300px}.el-dialog__footer{background-color:#fff}.app-margin-right-30[data-v-3e5595ab]{margin-right:30px}.my-list-tabs[data-v-3e5595ab]{border-radius:var(--el-card-border-radius);border:1px solid var(--el-card-border-color)}.list-left[data-v-3e5595ab]{padding-right:15px;display:flex;flex:1;align-items:stretch;justify-content:start}.list-number[data-v-3e5595ab]{margin-right:5px;color:#909399}.list-title[data-v-3e5595ab]{flex:1;font-weight:400}.list-right[data-v-3e5595ab]{min-width:70px}.list-right .el-badge.item[data-v-3e5595ab]{margin-right:30px}.list li[data-v-3e5595ab]{margin-bottom:5px;padding-bottom:5px;font-size:13px;line-height:30px;display:flex;align-items:flex-start;justify-content:space-between;border-bottom:1px solid #ebeef5}.list li[data-v-3e5595ab]:hover{background-color:#f5f5f5}.quickReplyLoginBox .tips[data-v-3e5595ab]{margin-left:50px;text-align:left;font-size:12px}.addReplyBox[data-v-3e5595ab]{margin-top:15px;padding-top:10px;border-top:1px dashed #ccc}.box-card .el-card__header[data-v-3e5595ab]{padding:10px 20px}.box-card .el-card__header span[data-v-3e5595ab]{font-size:14px}.clearfix[data-v-3e5595ab]:before,.clearfix[data-v-3e5595ab]:after{display:table;content:""}.clearfix[data-v-3e5595ab]:after{clear:both}.el-pagination[data-v-3e5595ab]{padding:15px 5px 0}.setBox .el-input__inner{margin-bottom:0;background-color:transparent;border:0;outline:none}.box-card .el-card__header[data-v-66d8ae03]{padding:10px 20px}.box-card .el-card__header span[data-v-66d8ae03]{font-size:14px}.setAIBox .el-input__inner{margin-bottom:0;background-color:transparent;border:0;outline:none}.margin-left{margin-left:15px} ');
+(a=>{const e=document.createElement("style");e.dataset.source="vite-plugin-monkey",e.textContent=a,document.head.append(e)})(' .quickReplyBox[data-v-f76f8183]{position:relative}.quickReplyBox .el-form[data-v-f76f8183]{text-align:left}.quickReplyBox .reply-form-inline .el-form-item[data-v-f76f8183]{margin-bottom:15px}.el-dialog__footer a{text-decoration:none}.el-tabs__nav-next,.el-tabs__nav-prev{height:44px;display:flex;justify-content:center;align-items:center}.el-dialog{display:flex;flex-direction:column;margin:0!important;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);max-height:calc(100% - 30px);max-width:1300px;min-width:1000px}.el-dialog__header{margin-right:0!important}.el-dialog__body{flex:1;overflow:auto;padding:0}.quickReplyBox .el-input__inner{margin-bottom:0;background-color:transparent;border:0;outline:none}#reply-control.open{height:calc(var(--composer-height) + 65px)!important}#reply-control .reply-area{height:calc(100% - 91px)!important}.app-dialog-foot[data-v-f76f8183]{color:#909399;font-size:14px}.quickReplyBoxTitle[data-v-f76f8183]{margin-right:10px;font-weight:700;color:red}.el-form-item--mini.el-form-item[data-v-f76f8183],.el-form-item--small.el-form-item[data-v-f76f8183]{margin-bottom:10px}.el-select[data-v-f76f8183]{width:300px}.el-dialog__footer{background-color:#fff}.app-margin-right-30[data-v-3e5595ab]{margin-right:30px}.my-list-tabs[data-v-3e5595ab]{border-radius:var(--el-card-border-radius);border:1px solid var(--el-card-border-color)}.list-left[data-v-3e5595ab]{padding-right:15px;display:flex;flex:1;align-items:stretch;justify-content:start}.list-number[data-v-3e5595ab]{margin-right:5px;color:#909399}.list-title[data-v-3e5595ab]{flex:1;font-weight:400}.list-right[data-v-3e5595ab]{min-width:70px}.list-right .el-badge.item[data-v-3e5595ab]{margin-right:30px}.list li[data-v-3e5595ab]{margin-bottom:5px;padding-bottom:5px;font-size:13px;line-height:30px;display:flex;align-items:flex-start;justify-content:space-between;border-bottom:1px solid #ebeef5}.list li[data-v-3e5595ab]:hover{background-color:#f5f5f5}.quickReplyLoginBox .tips[data-v-3e5595ab]{margin-left:50px;text-align:left;font-size:12px}.addReplyBox[data-v-3e5595ab]{margin-top:15px;padding-top:10px;border-top:1px dashed #ccc}.box-card .el-card__header[data-v-3e5595ab]{padding:10px 20px}.box-card .el-card__header span[data-v-3e5595ab]{font-size:14px}.clearfix[data-v-3e5595ab]:before,.clearfix[data-v-3e5595ab]:after{display:table;content:""}.clearfix[data-v-3e5595ab]:after{clear:both}.el-pagination[data-v-3e5595ab]{padding:15px 5px 0}.setBox .el-input__inner{margin-bottom:0;background-color:transparent;border:0;outline:none}.box-card .el-card__header[data-v-66d8ae03]{padding:10px 20px}.box-card .el-card__header span[data-v-66d8ae03]{font-size:14px}.setAIBox .el-input__inner{margin-bottom:0;background-color:transparent;border:0;outline:none}.margin-left{margin-left:15px} ');
 
 (function (vue, ElementPlus, ElementPlusIconsVue) {
   'use strict';
@@ -136,6 +138,11 @@
               currentPlatform.value = "discuz";
             } else if ($generator.content.indexOf("Discourse") > -1) {
               currentPlatform.value = "discourse";
+            }
+          } else {
+            let $html = document.body.innerHTML;
+            if ($html.indexOf("flarum-loading") > -1) {
+              currentPlatform.value = "flarum";
             }
           }
         }
@@ -282,6 +289,8 @@
           enterDiscourseEmberReply();
         } else if (currentPlatform.value == "v2ex") {
           enterReplyContentReply();
+        } else if (currentPlatform.value == "flarum") {
+          enterFlarumTextEditorReply();
         } else {
           enterFastPostReply();
         }
@@ -310,6 +319,17 @@
         }
         if (submitNow.value && !useAI.value && currentReply.value) {
           document.querySelector('#reply-box input[type="submit"]').click();
+          currentReply.value = "";
+        }
+      }
+      function enterFlarumTextEditorReply() {
+        let $textEditor = document.querySelector("textarea.FormControl.TextEditor-editor");
+        if ($textEditor) {
+          $textEditor.value = currentReply.value;
+          $textEditor.dispatchEvent(new Event("change"));
+        }
+        if (submitNow.value && !useAI.value && currentReply.value) {
+          document.querySelector(".item-submit button.Button").click();
           currentReply.value = "";
         }
       }
@@ -477,7 +497,7 @@ Tips：使用AI就像开盲盒，请准备好是否接受结果再提交`
       };
     }
   };
-  const App = /* @__PURE__ */ _export_sfc(_sfc_main$3, [["__scopeId", "data-v-029dc477"]]);
+  const App = /* @__PURE__ */ _export_sfc(_sfc_main$3, [["__scopeId", "data-v-f76f8183"]]);
   const _withScopeId = (n) => (vue.pushScopeId("data-v-3e5595ab"), n = n(), vue.popScopeId(), n);
   const _hoisted_1$2 = { class: "setBox" };
   const _hoisted_2$1 = {
@@ -2082,18 +2102,19 @@ Tips：使用AI就像开盲盒，请准备好是否接受结果再提交`
     }
   };
   const http = function(api, data, method) {
+    method = method ? method.toLowerCase() : "post";
     return new Promise((resolve, reject) => {
       let url = `https://quickreply.bmqy.net/api${api}`;
-      if (method && method.toLowerCase() === "get") {
+      if (method === "get") {
         url += `?${Object.keys(data).map((key) => key + "=" + encodeURIComponent(data[key])).join("&")}`;
       }
       _GM_xmlhttpRequest({
-        method: method ? method : "post",
+        method,
         url,
         headers: {
           "Content-Type": "application/json; charset=utf-8"
         },
-        data: `${JSON.stringify(data)}`,
+        data: method === "post" ? `${JSON.stringify(data)}` : "",
         responseType: "json",
         onload: function(xhr) {
           if (xhr.status == 200) {
@@ -2375,6 +2396,12 @@ Tips：使用AI就像开盲盒，请准备好是否接受结果再提交`
       if ($replyBox) {
         $appRoot.style.padding = "15px 15px 0";
         $replyBox.insertBefore($appRoot, $replyBox.childNodes[2]);
+      }
+      const $composerBox = document.querySelector("#composer");
+      if ($composerBox) {
+        const $composerInner = $composerBox.querySelector(".Composer");
+        $appRoot.style.padding = "15px 15px 0";
+        $composerInner.insertBefore($appRoot, $composerInner.childNodes[2]);
       }
       return $appRoot;
     })()
